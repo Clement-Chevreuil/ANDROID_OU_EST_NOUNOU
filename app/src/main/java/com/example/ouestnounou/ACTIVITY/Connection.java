55976c4201bd05c98264378d20d30a980884af40
Clement <clement.chevreuil@icloud.com>
2023-02-19 11:52:01 +0100

    Add Login, Navigation System

---
+++ app/src/main/java/com/example/ouestnounou/ACTIVITY/Connection.java
@@ -0,0 +1,21 @@
+package com.example.ouestnounou.ACTIVITY;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ouestnounou.R;
+
+
+public class Connection extends AppCompatActivity {
+
+    public Connection() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_connection);
+    }
+}
